ac22bcfd43590dec31ae972c14453c6c4ff4497b
Shaik <azmathulla.b.shaik@accenture.com>
2021-10-22 13:05:29 +0530

    first commit

---
+++ MyFirstRound-stackoverflow/app/src/main/java/com/example/myfirstround/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myfirstround
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myfirstround.Adapter.MainActivityAdapter
+import com.example.myfirstround.ViewModel.GetViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+
+//"@AndroidEntryPoint" -- iska matlab jitni b dependency provide hui iss app k ander woh yaha main activity me use ker sakte hain
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private val getViewModel:GetViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        getViewModel.response.observe(this, Observer { response->
+            recyclerView.adapter =MainActivityAdapter(response)
+        })
+
+
+
+
+    }
+}
